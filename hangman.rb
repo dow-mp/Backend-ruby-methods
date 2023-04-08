4083fbd881319a0b70f9b441002228fec79809de
dow-mp <derin.mitchell@gmail.com>
2023-04-07 23:13:39 -0400

    write hangman game

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,19 @@
 # Write a program hangman.rb that contains a function called hangman.  The function's parameters are a word and an array of letters.  It returns a string showing the letters that have been guessed.  Call the function from within your program so that you know that it works.
 # Example: hangman("bob",["b"]) should evaluate to "b_b"
 # Example: hangman("alphabet",["a","h"]) should return "a__ha___"
+
+def hangman(word, arr)
+    newArr = []
+    word.split("").each do |char| 
+        if arr.include?(char)
+            newArr.push(char)
+        else 
+            newArr.push("_")
+        end
+    end
+    puts newArr.join("")
+end
+
+hangman("alphabet",["a","h"])
+hangman("bob",["b"])
+hangman("lincoln",["n", "l"])
