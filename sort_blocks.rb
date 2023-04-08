015e18a2da9f6eb0a64c5c2e3bde6301176d1621
dow-mp <derin.mitchell@gmail.com>
2023-04-07 23:50:44 -0400

    sort based on params provided

--- sort_blocks.rb
+++ sort_blocks.rb
@@ -1,6 +1,5 @@
 # The ruby array sort method can use a block to sort various arrays.  The block must compare between two elements of the array, a and b.  If a<b, it should evaluate to -1.  If a==b, it should evaluate to 0.  And if a>b, it should evaluate to 1. Here is the start of a program, which you should call sort_blocks.rb.
 
-ruby
 class Book
   attr_reader :author, :title, :count
   def initialize(author,title,count)
@@ -24,13 +23,13 @@ new_array = book_array.sort do |a,b|
   author1 = a.author.downcase
   author2 = b.author.downcase
   author1 <=> author2
-  if author1 > author2
-    1
-  elsif author1 < author2
-    -1
-  else
-    0
-  end
+  # if author1 > author2
+  #   1
+  # elsif author1 < author2
+  #   -1
+  # else
+  #   0
+  # end
 end
 puts new_array
 
@@ -38,3 +37,27 @@ puts new_array
 # Note the use of the spaceship operator <=>.  It does what the commented out lines do, but on one line.
 
 # Add to this program. Add additional calls to book_array.sort that pass blocks.  For your first call to sort, pass a block so that the array is sorted in order of title, and print out the array.  For your second call to sort, pass a block so that the array is sorted in order of copies, and print out the array.
+
+
+60.times {print "-"}
+puts "\n"
+puts "Sorting alphabetically by title"
+
+two_array = book_array.sort do |a,b|
+  title1 = a.title.downcase
+  title2 = b.title.downcase
+  title1 <=> title2
+end
+puts two_array
+
+
+60.times {print "-"}
+puts "\n"
+puts "Sorting by number of copies"
+
+count_array = book_array.sort do |a,b|
+  count1 = a.count
+  count2 = b.count
+  count1 <=> count2
+end
+puts count_array
