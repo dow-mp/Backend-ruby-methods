e44a73381c34ff9abee5f9ee96174bcf3c4ca997
dow-mp <derin.mitchell@gmail.com>
2023-04-10 20:43:33 -0400

    complete sums classes with methods

--- sums.rb
+++ sums.rb
@@ -4,6 +4,34 @@
 
 # attr_accessor :total
 
+
+class Sum1
+    attr_accessor :total
+
+    def initialize(a, b)
+        @total = a + b
+    end
+end
+
 # (b) a class called Sum2 with an initialize method that takes two parameters a and b and sets the instance variable a to the parameter a and the instance variable b to the parameter b. Create a method new_total inside Sum2 that returns the sum of the instance variables a and b.
 
+class Sum2
+
+    def initialize(a, b)
+        @a = a
+        @b = b
+    end
+
+    def new_total
+        sum = @a + @b
+    end
+end
+
+
 # (c) In the mainline program, create instances of Sum1 and Sum2, passing 5 and 6 as parameters on the new statement.  Print out the total for Sum1.  Print out the new_total for Sum2.
+
+test1 = Sum1.new(5,6)
+test2 = Sum2.new(5,6)
+
+puts test1.total
+puts test2.new_total
